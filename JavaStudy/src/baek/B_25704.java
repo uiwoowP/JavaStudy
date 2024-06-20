32888dd8251dd3bac8c69d91a497cc11dedce310
i7 <i7@i7A-67>
2024-06-20 17:22:40 +0900

    B_25704

---
+++ JavaStudy/src/baek/B_25704.java
@@ -0,0 +1,18 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_25704 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int stamp = Integer.parseInt(br.readLine());
+		int price = Integer.parseInt(br.readLine());
+		
+		
+		
+	}
+
+}
